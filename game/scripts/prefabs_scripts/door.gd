2872c6ff0d6d71e32e6367bd3061fa05eb5606cf
Erik Felipe Viana Rocha <erikviana32@gmail.com>
2024-09-10 12:54:24 -0400

    att. 10/09/24: fix needed in the room creation

--- game/scripts/prefabs_scripts/door.gd
+++ game/scripts/prefabs_scripts/door.gd
@@ -1,13 +1,19 @@
 extends Area2D
 
-const room_instance := preload("res://prefabs/room.tscn")
+class_name Door
 
 var door_value := 0
 
+const room_instance := preload("res://prefabs/room.tscn")
+
 @onready var room_spawner := $room_spawn as Marker2D
+@onready var door_detector := $door_detector as RayCast2D
+
+func _ready() -> void:
+	pass
 
 func _on_body_entered(body: Node2D) -> void:
-	pass
+	spawn_room()
 
 func direction_setter(direction):
 	if direction == "up":
@@ -27,4 +33,9 @@ func direction_setter(direction):
 		set_rotation_degrees(180)
 		room_spawner.set_position(Vector2(0, -191))
 		
-	print(room_spawner.get_position())
+
+func spawn_room():
+	print(room_instance)
+	var room := room_instance.instantiate()
+	get_parent().call_deferred("add_child", room)
+	room.global_position = room_spawner.position
--- game/scripts/prefabs_scripts/room.gd
+++ game/scripts/prefabs_scripts/room.gd
@@ -3,17 +3,23 @@ extends CharacterBody2D
 const door_instance := preload("res://prefabs/door.tscn")
 
 var rng_door := RandomNumberGenerator.new()
+var door_instances := []
 
 @export var room_event := Event
-@export var entered_door := 1
+@export var entered_door := 0
 
 @onready var right_door_spawn := $right_door_spawn as Marker2D
 @onready var left_door_spawn := $left_door_spawn as Marker2D
 @onready var upper_door_spawn := $upper_door_spawn as Marker2D
 @onready var lower_door_spawn := $lower_door_spawn as Marker2D
 
+@onready var upper_door_detector := $upper_door_detector as RayCast2D
+@onready var lower_door_detector := $lower_door_detector as RayCast2D
+@onready var right_door_detector := $right_door_detector as RayCast2D
+@onready var left_door_detector := $left_door_detector as RayCast2D
+
 func _ready() -> void:
-	room_doors_instances()
+	pass
 	
 func _process(delta: float) -> void:
 	pass
@@ -36,21 +42,29 @@ func room_doors_instances():
 		direction = "up"
 		created_door.global_position = upper_door_spawn.position
 		created_door.call_deferred("direction_setter", direction)
+		door_instances.append(created_door)
 		
 	if right == 1 or entered_door == 2:
 		var created_door = room_door_creator()
 		direction = "right"
 		created_door.global_position = right_door_spawn.position
 		created_door.call_deferred("direction_setter", direction)
+		door_instances.append(created_door)
 		
 	if left == 1 or entered_door == 3:
 		var created_door = room_door_creator()
 		direction = "left"
 		created_door.global_position = left_door_spawn.position
 		created_door.call_deferred("direction_setter", direction)
+		door_instances.append(created_door)
 		
 	if down == 1 or entered_door == 4:
 		var created_door = room_door_creator()
 		direction = "down"
 		created_door.global_position = lower_door_spawn.position
 		created_door.call_deferred("direction_setter", direction)
+		door_instances.append(created_door)
+
+
+func _on_insider_room_area_body_entered(body: Node2D) -> void:
+	room_doors_instances()
